5e50249d479ef332e785422994300a8328f96282
Nico McD <35404943+nicomcd@users.noreply.github.com>
2019-10-01 18:37:25 -0400

    Create names.java

---
+++ names.java
@@ -0,0 +1,11 @@
+class Lesson_3_Activity_One{
+ public static void main(String args[]){
+    Scanner scan = new Scanner (System.in);
+    String n;
+    System.out.println("What is your name?");
+    n = scan.nextLine();
+    System.out.println("Hi " + n + ", nice to see you.");
+  }   
+    
+}
+  
