c22f87738bf7b7428ade4cfa42c525138c2ee333
Nico McD <35404943+nicomcd@users.noreply.github.com>
2019-10-01 18:44:42 -0400

    Create adj and name.java

---
+++ adj and name.java
@@ -0,0 +1,14 @@
+class Lesson_3_Activity_Three{
+ public static void main(String args[]){
+    Scanner scan = new Scanner (System.in);
+    String name;
+    String adj;
+    System.out.println("Hi there. What is your name?");
+    name = scan.nextLine();
+    System.out.println("What adjective describes you?");
+    adj = scan.nextLine();
+    System.out.println("My name is "+ name + ". I am " + adj + ".");
+    }   
+    
+}
+  
