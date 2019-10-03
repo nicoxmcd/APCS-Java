45be47c97823667689fb865caa5f3eded0efcd82
Nico McD <35404943+nicomcd@users.noreply.github.com>
2019-10-03 12:31:39 -0400

    Create Lesson_5_Activity_One.java

---
+++ Lesson_5_Activity_One.java
@@ -0,0 +1,15 @@
+class Lesson_5_Activity_One{
+    public static void main(String args[]){
+	    Scanner scan = new Scanner (System.in);
+        double number;
+        double num;
+        double difference;
+        System.out.println("Enter two numbers:");
+	    number = scan.nextDouble();
+        num = scan.nextDouble();
+        difference = number - num;
+        System.out.println(difference);
+        
+ }   
+    
+}
