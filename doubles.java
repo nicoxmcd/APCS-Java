f2d57b18a396690e33daaf304ecab7bbe80005fa
Nico McD <35404943+nicomcd@users.noreply.github.com>
2019-10-03 10:13:21 -0400

    Create doubles.java

---
+++ doubles.java
@@ -0,0 +1,12 @@
+class Lesson_4_Activity_One{
+ public static void main(String args[]){
+	Scanner scan = new Scanner (System.in);
+  double number;
+  double num;
+  System.out.println("Enter two numbers:");
+	number = scan.nextDouble();
+  num = scan.nextDouble();
+  System.out.println(num+" "+number);
+ }   
+    
+}
