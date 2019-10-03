da3ec9469b8fec068edec516489d1962939c0dea
Nico McD <35404943+nicomcd@users.noreply.github.com>
2019-10-03 12:47:39 -0400

    Create Lesson_5_Activity_Two.java

---
+++ Lesson_5_Activity_Two.java
@@ -0,0 +1,19 @@
+class Lesson_5_Activity_Two{
+    public static void main(String args[]){
+	    Scanner scan = new Scanner (System.in);
+        int num1;
+        int num2;
+        int num3;
+        int num4;
+        int sum;
+        System.out.println("Enter four numbers:");
+	    num1 = scan.nextInt();
+        num2 = scan.nextInt();
+        num3 = scan.nextInt();
+        num4 = scan.nextInt();
+        sum = num1 + num2 + num3 + num4;
+        System.out.println(sum);
+        
+ }   
+    
+}
