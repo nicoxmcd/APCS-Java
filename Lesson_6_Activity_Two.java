967f0c7a3b9991b410d0993295223f432cbd9427
Nico McD <35404943+nicomcd@users.noreply.github.com>
2019-10-07 17:32:55 -0400

    Create Lesson_6_Activity_Two.java

---
+++ Lesson_6_Activity_Two.java
@@ -0,0 +1,13 @@
+class Lesson_6_Activity_Two{
+    public static void main(String args[]){
+	    Scanner scan = new Scanner (System.in);
+        double z;
+        int x;
+        System.out.println("Please input a decimal number:");
+        z = scan.nextDouble();
+        x = (int)z; 
+        System.out.println("Answer: " + x);
+        
+ }   
+    
+}
