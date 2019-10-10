039fa958a5ef66c1bcc9af4dc8b7082b13ab7eab
Nico McD <35404943+nicomcd@users.noreply.github.com>
2019-10-10 12:23:26 -0400

    Create Lesson_9_Activity_One.java

---
+++ Lesson_9_Activity_One.java
@@ -0,0 +1,14 @@
+import java.lang.Math;
+
+// class Main {
+class Lesson_9_Activity_One {
+    public static void main(String[] args) {
+      
+    double answer1 = Math.random();
+    int range = 10;
+    int min = 1;
+    double answer2 = (int)(answer1*range) + min;
+    int answer3 = (int)answer2;
+    System.out.println(answer3);
+    }
+}
