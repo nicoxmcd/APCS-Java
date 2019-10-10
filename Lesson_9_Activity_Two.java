611f386890da0283d4281ba101ac8fb22b27e77f
Nico McD <35404943+nicomcd@users.noreply.github.com>
2019-10-10 12:30:24 -0400

    Create Lesson_9_Activity_Two.java

---
+++ Lesson_9_Activity_Two.java
@@ -0,0 +1,9 @@
+import java.lang.Math;
+
+// class Main {
+class Lesson_9_Activity_Two {
+    public static void main(String[] args) {
+      
+    System.out.println((int)(Math.random()*41) + -20);
+    }
+}
