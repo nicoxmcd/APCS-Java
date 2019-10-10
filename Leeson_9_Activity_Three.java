918202696a0879a139b6d62d67da0ddd122141bb
Nico McD <35404943+nicomcd@users.noreply.github.com>
2019-10-10 12:32:09 -0400

    Update and rename Leeson_9_Activity_Two.java to Leeson_9_Activity_Three.java

--- Leeson_9_Activity_Three.java
+++ Leeson_9_Activity_Three.java
@@ -1,9 +1,9 @@
 import java.lang.Math;
 
 // class Main {
-class Lesson_9_Activity_Two {
+class Lesson_9_Activity_Three {
     public static void main(String[] args) {
       
-    System.out.println((int)(Math.random()*41) + -20);
+    System.out.println((int)(Math.random()*21) + 20);
     }
 }
