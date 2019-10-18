e04db6c6796f583175afeead4c175a4a1638d857
Nico McD <35404943+nicomcd@users.noreply.github.com>
2019-10-18 12:16:38 -0400

    Create Lesson_11_Activity_One.java

---
+++ Lesson_11_Activity_One.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class Lesson_11_Activity_One {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a whole number:");
+		int inin = scan.nextInt();
+		if(inin == 176){
+			System.out.println("YES");
+		}
+	}
+}
