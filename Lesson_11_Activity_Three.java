89c422e50b584692a205013b8eddf64460f4197c
Nico McD <35404943+nicomcd@users.noreply.github.com>
2019-10-18 12:21:34 -0400

    Rename Lesson_3_Activity_Three.java to Lesson_11_Activity_Three.java

---
+++ Lesson_11_Activity_Three.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class Lesson_11_Activity_Three {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter two whole numbers:");
+		int inin = scan.nextInt();
+		int ininin = scan.nextInt();
+		if(inin == ininin){
+			System.out.println("YES");
+		}
+	}
+}
