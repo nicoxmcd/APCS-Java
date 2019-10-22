c825b89c791a2222d9a399f75810f15fa454a900
Nico McD <35404943+nicomcd@users.noreply.github.com>
2019-10-22 08:32:24 -0400

    Create Lesson_12_Activity_Three.java

---
+++ Lesson_12_Activity_Three.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class Lesson_12_Activity_Three {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a test score: ");
+		double a = scan.nextDouble();
+		if (Math.abs(a) <= 100){
+			System.out.println("Valid");
+		} else {
+			System.out.println("Not Valid");
+		}
+
+		
+	}
+}
