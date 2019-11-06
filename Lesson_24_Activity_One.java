22ab9cbcb6cdebc04f23c56007c80dc3220df998
Nico McD <35404943+nicomcd@users.noreply.github.com>
2019-11-06 00:38:34 -0500

    Create Lesson_24_Activity_One.java

---
+++ Lesson_24_Activity_One.java
@@ -0,0 +1,14 @@
+import java.lang.Math;
+
+class Lesson_24_Activity_One {
+  public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		for (int i = 23; i <= 89; i++) {
+		System.out.print(i + " ");
+      if (0 == (i - 23 + 1) % 10) {
+        System.out.println("");
+      }
+
+}
+	}
+}
