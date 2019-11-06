53ce641802222183673f33376ce54808431961f2
Nico McD <35404943+nicomcd@users.noreply.github.com>
2019-11-06 00:43:34 -0500

    Create Lesson_24_Activity_Two.java

---
+++ Lesson_24_Activity_Two.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class Lesson_24_Activity_Two {
+  public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		for (int i = 2; i <= 50; i += 2) {
+			System.out.println(i);
+    
+}
+	}
+}
