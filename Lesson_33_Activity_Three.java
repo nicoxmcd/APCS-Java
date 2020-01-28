73d7c6676f7875a17b64bd4cf7eb8ae30bf37876
Nico McD <35404943+nicomcd@users.noreply.github.com>
2020-01-28 09:48:42 -0500

    Create Lesson_33_Activity_Three.java

---
+++ Lesson_33_Activity_Three.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+class Lesson_33_Activity_Three {
+  public static void printit(int list[]){
+    for (int i=0; i<list.length; i++){
+      System.out.print(list[i] + " ");
+    }
+  }
+    
+    public static void main(String[] args)
+     {
+    }
+}
