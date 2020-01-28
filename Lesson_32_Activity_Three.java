96fb78a6dec2777d96890f220dc89a3dde9b7f42
Nico McD <35404943+nicomcd@users.noreply.github.com>
2020-01-28 00:02:41 -0500

    Create Lesson_32_Activity_Three.java

---
+++ Lesson_32_Activity_Three.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.lang.Math; 
+class Lesson_32_Activity_Three {
+ 
+  public static void swap(int first, int second){
+   System.out.println(second + " " + first);
+   
+   }
+  public static void main(String[] args){
+ Scanner scan = new Scanner(System.in);
+int first = scan.nextInt();
+int second = scan.nextInt();
+
+ 
+  swap(first,second);
+ 
+ 
+ 
+
+   
+    
+  }
+}
