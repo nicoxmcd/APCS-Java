aece0c06f8050d0484cfd531f5d36a1c75813777
Nico McD <35404943+nicomcd@users.noreply.github.com>
2020-01-28 09:46:18 -0500

    Create Lesson_33_Activity_Two.java

---
+++ Lesson_33_Activity_Two.java
@@ -0,0 +1,17 @@
+
+
+import java.util.Scanner;
+
+class Lesson_33_Activity_Two {
+  public static void randomize(int list[]){
+    for (int i=0; i<list.length; i++){
+      list[i]=(int)(Math.random()*90)+10;
+    }
+    
+  }
+  
+    public static void main(String[] args){
+     
+     
+    }
+}
